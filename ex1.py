7147962bbb2b4df4c3605d09bacdb2f1a24c9537
Sapna Badola <31178713+sapnaxapzz@users.noreply.github.com>
2020-12-09 20:32:07 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,13 @@
+def ruler(x):
+    i = 1
+    s1 = ""
+    s2 = " "
+    while (i <= x):
+        s1 += str(i % 10)
+        i += 1
+        if (i % 10 == 0):
+            s2 += str(int(i / 10))
+        else:
+            s2 += " "
+    print(s2)
+    print(s1)
